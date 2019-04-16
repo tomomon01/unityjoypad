54488393887a5cccef8759bd8476a462fd4ff70d
tomomon <tomomon@tomomonnoMacBook-Air.local>
2019-04-17 00:25:53 +0900

    first commit

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MoveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        this.transform.position += new Vector3(x, y);
+        		
+	}
+}
